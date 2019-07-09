ce55f2aabae4c8247c56bf4256fef1e21633328f
Fluoextine <cyc3095689263@gmail.com>
2019-07-09 21:17:50 +0800

    提交

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //我给你示范一下ok
+
+        for(int i=49999;i<=499999;i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+
+        }
     }
 }
+//这个项目可以在这写ok你只要写完然后提交以下，我这边就能收到你提交的内容咋提交看着ok
